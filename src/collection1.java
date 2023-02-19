de5d819a6cef0c0bc6a0682efa28e4d5e5cbc189
fujiwaranaohiro <fujiwaranaohiro@localhost>
2023-02-19 16:14:59 +0900

    コレクション・リスト

---
+++ src/collection1.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class collection1 {
+	public static void main(String[] args) {
+		ArrayList<String> fruits;
+		fruits = new ArrayList();
+		
+		fruits.add("りんご");
+		fruits.add("みかん");
+		
+		System.out.println("fruits");
+	}
+}
