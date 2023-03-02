fe98f46a7b4281d3ee6bf32624fb7a5f6930c1fb
fujiwaranaohiro <fujiwaranaohiro@localhost>
2023-03-02 11:13:57 +0900

    BMIキーボード入力

--- src/naohiro/javastudy/BmiCalc3.java
+++ src/naohiro/javastudy/BmiCalc3.java
@@ -1,22 +1,28 @@
 package naohiro.javastudy;
 
+import java.util.Scanner;
 
-
-public class BmiCalc {
+public class BmiCalc3 {
 //BMI体格異数を計算するプログラム
 //BMI = 体重　/ (身長　* 身長)
 //体重 kg 身長　m→cm
 	public static void main(String[] args) {
 		double weight,height,bmi;
 
-		weight = 62;
+//		weight = 60;
 //		height = 1.71;
-		height = 171;
+//		height = 171;
 
+		Scanner stdIn = new Scanner(System.in);
+		System.out.println("体重は？(kg)　：");
+		weight = stdIn.nextDouble();
+		System.out.println("身長は？(cm)　：");
+		height = stdIn.nextDouble();
 		height /= 100;
+
 		bmi = weight / (height * height);
 
-		System.out.println(bmi);
+		System.out.println("あなたのBMIは、" + bmi + "です。");
 
 		if (bmi < 18.5) {
 			System.out.println("痩せ型です");
