dbcae0266ec089d552bb2cec7d65394ad8bd10c9
fujiwaranaohiro <fujiwaranaohiro@localhost>
2023-03-05 22:33:04 +0900

    this/progete

---
+++ src/naohiro/javastudy/Person.java
@@ -0,0 +1,12 @@
+package naohiro.javastudy;
+
+public class Person {
+
+	public String name;
+
+	public void hello() {
+		System.out.println("こんにちは、私は" + this.name + "です");
+
+	}
+
+}
---
+++ src/naohiro/javastudy/Person2.java
@@ -0,0 +1,17 @@
+package naohiro.javastudy;
+
+public class Person2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Person person1 = new Person();
+	    person1.name = "Kate Jones";
+	    person1.hello();
+
+	    Person person2 = new Person();
+	    person2.name = "John Christopher Smith";
+	    person2.hello();
+
+	}
+
+}
