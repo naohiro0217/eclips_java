b4fb02eb98636b9ce23d08cd7b9da0863d68cdf8
fujiwaranaohiro <fujiwaranaohiro@localhost>
2023-03-07 14:55:48 +0900

    論理和

---
+++ src/naohiro/javastudy/CommonMultiple2.java
@@ -0,0 +1,18 @@
+package naohiro.javastudy;
+
+public class CommonMultiple2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+//		２まth３の倍数を、１から５０未満の範囲で表示する
+		int i = 1;
+		while (i < 50) {
+			if ((i % 2 == 0) || (i % 3 == 0)) {
+				System.out.println(i);
+			}
+			i++; //インクリメント演算子
+		}
+
+	}
+
+}
