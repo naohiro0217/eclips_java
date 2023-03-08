1e1b741d96c3073f0be9eae362d20a748012993b
fujiwaranaohiro <fujiwaranaohiro@localhost>
2023-03-08 10:20:23 +0900

    NOT演算子

--- src/naohiro/javastudy/CommonMultiple3.java
+++ src/naohiro/javastudy/CommonMultiple3.java
@@ -1,13 +1,14 @@
 package naohiro.javastudy;
 
-public class CommonMultiple2 {
+public class CommonMultiple3 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-//		２と３の倍数を、１から５０未満の範囲で表示する
+//		２と３の倍数を以外の数を表示する
 		int i = 1;
-		while (i < 50) {
-			if ((i % 2 == 0) || (i % 3 == 0)) {
+		while (i < 20) {
+			if (!((i % 2 == 0) && (i % 3 == 0))) {
+				//NOT演算子の適用例
 				System.out.println(i);
 			}
 			i++; //インクリメント演算子
