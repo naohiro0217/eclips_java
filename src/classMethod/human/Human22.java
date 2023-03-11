27553a7a3966b64a021230f314bb4069ffbbf760
fujiwaranaohiro <fujiwaranaohiro@localhost>
2023-03-11 17:01:33 +0900

    クラスとメソッド

---
+++ src/classMethod/human/Human22.java
@@ -0,0 +1,16 @@
+package classMethod.human;
+
+public class Human22 {
+	public String name;
+	public int age;
+
+	public Human22() {
+		name = "山田";
+		age = 20;
+	}
+
+	public Human22(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
