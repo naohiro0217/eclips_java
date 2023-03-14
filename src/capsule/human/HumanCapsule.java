15ab78d0f03c3d35bf3c550e34f228751676ffc5
fujiwaranaohiro <fujiwaranaohiro@localhost>
2023-03-14 16:56:22 +0900

    カプセル化

---
+++ src/capsule/human/HumanCapsule.java
@@ -0,0 +1,29 @@
+package capsule.human;
+
+public class HumanCapsule {
+	private String name;
+	private int age;
+	
+	public HumanCapsule(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+}
