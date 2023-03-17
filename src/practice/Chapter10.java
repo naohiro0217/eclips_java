c49daef876a687f24862a9819f794c25ebe3ab00
fujiwaranaohiro <fujiwaranaohiro@localhost>
2023-03-17 23:33:07 +0900

    例外

--- src/practice/Chapter10.java
+++ src/practice/Chapter10.java
@@ -34,6 +34,8 @@ public class Chapter10 {
         System.out.println("インデックス " + index + " の要素は " + array[index] + " です");
     }
 
+
+
     public static void throwSQLException() throws SQLException {
         throw new SQLException("SQLエラーです");
     }
