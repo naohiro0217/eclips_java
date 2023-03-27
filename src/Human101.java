4802e783288fcd62698ee55d2fc3e2bde348ba51
fujiwaranaohiro <fujiwaranaohiro@localhost>
2023-03-27 17:52:01 +0900

    ９章復習１

---
+++ src/Human101.java
@@ -0,0 +1,16 @@
+
+public class Human101 {
+	public String name;
+	public int age;
+
+	public Human101() {
+		name = "山田";
+		age = 20;
+	}
+
+	public Human101(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+}
