bcbea50b26f5225c1b1e41deeb81e50f922b1b94
fujiwaranaohiro <fujiwaranaohiro@localhost>
2023-04-14 12:40:00 +0900

    クラスとオブジェクト

---
+++ src/SampleClass601.java
@@ -0,0 +1,16 @@
+
+public class SampleClass601 {
+	int n = 10;
+    String s = "field";
+
+    int add(int a,int b){
+        return a + b;
+    }
+    String add(String s){
+        return this.s + s;
+    }
+    void showNum(){
+        System.out.println("n = " + n);
+    }
+
+}
