ce8dc9eb7a5b3e54c3920e2ec7b3ee71df8fad2a
fujiwaranaohiro <fujiwaranaohiro@localhost>
2023-04-19 22:04:04 +0900

    アクセス指定子

---
+++ src/Sample701.java
@@ -0,0 +1,15 @@
+
+public class Sample701 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		SampleClass02 s = new SampleClass02();
+
+        s.method2();
+
+        s.method3();
+
+
+	}
+
+}
